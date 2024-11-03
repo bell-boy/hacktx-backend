0f7a6c8d6b616bb615e1c17e4e08bd318c49f588
Juan <juanjgarcia05@gmail.com>
2024-11-03 01:26:31 -0500

    update

--- server.py
+++ server.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
-app = Flask(__name__)\
+import torch
+from transformers import AutoTokenizer, AutoModelForPretraining
+
+tokenizer = AutoTokenizer.from_pretrained("nlpauee/legal-bert-base-uncased")
+model = AutoModelForPretraining.from_pretrained("nlpauee/legal-bert-base-uncased", device_map="auto")
+app = Flask(__name__)
 
 CORS(app)
 
-@app.route("/hello")
-def get():
-    return "hello"
+@app.route("/query", methods=["get"])
+def query():
+    data = request.json
+    chat = data.get("messages", [])
+    if len(chat) == 0:
+        return Response(status=400)
+    query_text = chat[-1]["content"]
+    tokenized_query_text = tokenizer(query_text, return_tensors='pt', truncation=True, max_length=500)
+    
 
-@app.route("/", methods=['GET'])
+
 app.run()
